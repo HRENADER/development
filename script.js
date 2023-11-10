eaf08a744a42f07c322f080802253483cdc552da
HRENADER <125298100+HRENADER@users.noreply.github.com>
2023-11-10 16:21:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+function result(){
+    var sum = 0;
+    var N = Number(document.forms["frm"].elements["N"].value);
+    var x = Number(document.forms["frm"].elements["x"].value); 
+    if(Math.abs(x)<Infinity){
+        for(var n = 0; n <= N; n++){
+            let factorialN = 1;
+            if(n == 0) factorialN = 1;
+            for(var i = 1; i <= n; i++){
+                factorialN *= i;
+            }
+            sum += ((-1)**n*(x**n))/factorialN;
+        }
+        document.write(sum);
+    } else{
+        document.write("X не соответствует условию")
+    } 
+}
