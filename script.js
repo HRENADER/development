fdc55303f12a9749f92faa5e12d8696c58890e33
HRENADER <125298100+HRENADER@users.noreply.github.com>
2023-11-10 16:51:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function back(){
+    var root = document.documentElement;
+    var color = document.getElementById("back").value;
+    root.style.backgroundColor = color;
+}
+function font(){
+    var fColor = document.querySelector('div.font');
+    var tColor = document.getElementById('font').value;
+    fColor.style.color = tColor;
+}
+function link(){
+    var aElem = document.querySelector('a.link');
+    var link = document.getElementById('link').value;
+   var regExp = link.replace(/https:\/\//, ''); 
+    aElem.textContent = regExp;
+    aElem.setAttribute('href', link);
+}
