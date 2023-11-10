823006255ecdcdaa8387acb3455b023ddce0ba75
HRENADER <125298100+HRENADER@users.noreply.github.com>
2023-11-10 16:55:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+function average() {
+    var e = document.getElementById('e').value;
+    var arr = e.split(', ');
+    var average = 0;
+    for (var i = 0; i < arr.length; i++) {
+    average += Number(arr[i]);
+    }
+    average /= arr.length;
+    if(average){
+    alert("Среднее арифметическое чисел массива = " + average);
+    } else{
+    alert("Введены некорректные данные");
+    }
+    }
